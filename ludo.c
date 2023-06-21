a58cbd98121976331213be5ca2764fd09a986334
italovini223 <italovinicius2018@gmail.com>
2023-06-21 16:15:57 -0300

    ✨ adicionado funções para jogar mas com bug

--- ludo.c
+++ ludo.c
@@ -7,19 +7,30 @@
 
 struct jogador {
     char nome[20];
-    int pecas[4], cor;
+    int pecas[4], cor, jogasRepetidas;
+    int pecasParaJogar[4]; 
 };
 
 
 int jogarDado();
 void preencheDadosDosJogadores( struct jogador *jogadores, int quantidade);
 bool verificaCor(int cor, struct jogador jogador[], int quantidadeDeJogadores);
+bool pecaMeixaEValida(struct jogador *jogador, int pecaSelecionada);
+bool varificaSeAVencedor(struct jogador jogadores[], int quantidadeDeJogadores);
+void zeraPosicaoDasPecas(struct jogador jogadores[], int qunatidadeDeJogadores);
+void zeraAsPecasQuePodemJogar(struct jogador jogadores[], int posicaoDoJogador);
+void MovimentaPeca(int quantidadeASerMexida, struct jogador jogadores[], int posicaoJogador, int pecaASerMexida);
+
 
 
 
 int main(){
     srand(time(NULL));
-    int tabuleiro[52], dado, quantidadeDeJogadores;
+    bool pecaEValida, haVencedor;
+    int tabuleiro[15][15];
+    int dado, quantidadeDeJogadores, possibilidadeDeJogadas = 0;
+    int pecaMexida;
+    char stringVazia[3];
 
     do {
         printf("Digite a quantidade de jogadorees (2 ou 4): ");
@@ -33,6 +44,79 @@ int main(){
     struct jogador jogadores[quantidadeDeJogadores];
 
     preencheDadosDosJogadores(jogadores, quantidadeDeJogadores);
+    zeraPosicaoDasPecas(jogadores, quantidadeDeJogadores);
+
+    for(int i = 0; haVencedor != true; i++){
+        jogadores[i].jogasRepetidas = 0;
+        possibilidadeDeJogadas = 0;
+        do{
+            zeraAsPecasQuePodemJogar(jogadores, i);
+            dado = jogarDado();
+            
+            printf("Jogador %s, voce triou %d no dado!\n ", jogadores[i].nome, dado);
+
+            if(dado == 6){
+
+                jogadores[i].jogasRepetidas++;
+
+                
+                for(int j = 0; j < 4; j++){
+                    if(jogadores[i].pecas[j] == 0){
+                        jogadores[i].pecasParaJogar[j] = jogadores[i].pecas[j+1];
+                        possibilidadeDeJogadas++;
+                    } else {
+                        jogadores[i].pecasParaJogar[j] = jogadores[i].pecas[j+1];
+                        possibilidadeDeJogadas++;
+                    }
+                }
+
+            } else {
+                for(int j = 0; j < 4; j++){
+                    if(jogadores[i].pecas[j] > 0){
+                       jogadores[i].pecasParaJogar[j] = jogadores[i].pecas[j+1];
+                       possibilidadeDeJogadas++;
+                    }
+                    
+                }
+
+            }
+
+            if(possibilidadeDeJogadas > 0){
+                for(int j = 0; i < 4; j++){
+                    if(jogadores[i].pecasParaJogar[j] > 0){
+                        printf("digite %d para mover a peca %d por %d casas!\n", jogadores[i].pecasParaJogar[j], jogadores[i].pecasParaJogar[j], dado);
+                    }
+                }
+
+                do {
+                    printf("Digite sua opcao: ");
+                    scanf("%d", &pecaMexida);
+
+                    pecaEValida = pecaMeixaEValida(&jogadores[i], pecaMexida);
+
+                    if(pecaEValida == false ){
+                        printf("\n\nEscolha uma peca Valida!\n\n");
+                    }
+
+                    MovimentaPeca(dado, jogadores, i, pecaMexida);
+                } while(pecaEValida != true);
+            } else {
+                printf("Voce nao pode movimentar nenhuma peca!\n");
+                printf("Aperte qualquer tecla para continuar para continuar");
+                setbuf(stdin, NULL);
+                gets(stringVazia);
+            }
+
+
+            
+        }while(dado == 6 && jogadores[i].jogasRepetidas < 3);
+
+        haVencedor = varificaSeAVencedor(jogadores, quantidadeDeJogadores);
+
+        if(i == quantidadeDeJogadores){
+            i == 0;
+        }
+    }
 
     
 }
@@ -41,7 +125,10 @@ int jogarDado(){
 
     int numero;
 
-    numero = rand()%6;
+    do {
+        numero = (rand()%6);
+    } while(numero == 0);
+    
 
     return numero;
 }
@@ -102,3 +189,74 @@ bool verificaCor(int cor, struct jogador jogadores[], int quantidadeDeJogadores)
     return corEValida;
 }
 
+void zeraPosicaoDasPecas(struct jogador jogadores[], int qunatidadeDeJogadores){
+    for(int i = 0; i < qunatidadeDeJogadores; i++){
+        for(int j = 0; j < 4; j++){
+            jogadores[i].pecas[j] = 0;
+        }
+    }
+}
+
+void zeraAsPecasQuePodemJogar(struct jogador jogadores[], int posicaoDoJogador){
+    for(int i = 0; i < 4; i++){
+        jogadores[posicaoDoJogador].pecasParaJogar[i] = 0;
+    }
+}
+
+void MovimentaPeca(int quantidadeASerMexida, struct jogador jogadores[], int posicaoJogador, int pecaASerMexida){
+
+    int PodicaoDaPeca;
+
+    jogadores[posicaoJogador].pecas[pecaASerMexida] += quantidadeASerMexida;
+
+    PodicaoDaPeca = jogadores[posicaoJogador].pecas[pecaASerMexida];
+
+
+    printf("A peca %d esta na posicao %d do tabuleiro agora\n", pecaASerMexida, jogadores[posicaoJogador].pecas[pecaASerMexida]);
+}
+
+bool pecaMeixaEValida(struct jogador *jogador, int pecaSelecionada){
+    bool pecaVailda;
+    int quantidadeDePecasValidas = 0;
+
+    for(int i = 0; i < 4; i++){
+        if(jogador->pecasParaJogar[i] == pecaSelecionada){
+            quantidadeDePecasValidas++;
+        }
+    }
+
+    if(quantidadeDePecasValidas == 1){
+        pecaVailda = true;
+    } else {
+        pecaVailda = false;
+    }
+
+    return pecaVailda;
+}
+
+bool varificaSeAVencedor(struct jogador jogadores[], int quantidadeDeJogadores){
+    bool haVencedor;
+    int pecasFinalizadas = 0;
+
+
+    for(int i = 0; i < quantidadeDeJogadores; i++){
+        pecasFinalizadas = 0;
+        for(int j = 0; j < 4; j++){
+            if(jogadores[i].pecas[j] == 56){
+                pecasFinalizadas++;
+
+                if(pecasFinalizadas == 4){
+                    break;
+                }
+            }
+        }
+
+        if(pecasFinalizadas == 4){
+            haVencedor = true;
+        } else{
+            haVencedor = false;
+        }
+    }
+
+    return haVencedor;
+}
